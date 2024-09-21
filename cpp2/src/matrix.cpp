d040cb2ffae86fa121db69f69fd0872a2eec6814
ChedyMeksi007 <mekchedy@gmail.com>
2024-09-21 20:12:26 +0200

    still experimenting

--- cpp2/src/matrix.cpp
+++ cpp2/src/matrix.cpp
@@ -59,7 +59,7 @@ void Matrix :: printToConsole(){
 double Matrix :: generateRandomNumber() {
 	std::random_device rd;
 	std::mt19937 gen(rd());
-	std::uniform_real_distribution<> dis(-2, 2);
+	std::normal_distribution<> dis(0.0,1.0);
 
 	return dis(gen);
 }
